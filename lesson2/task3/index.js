9462189711a929c267a8b602d06d9e05ef5aa681
HardCode666 <hard.drive2036@gmail.com>
2020-06-11 21:27:28 +0300

    commit

--- lesson2/task3/index.js
+++ lesson2/task3/index.js
@@ -1,11 +1,17 @@
 let goodNumber = 17;
-let anotherNumber = 77;
+let anotherNumber = goodNumber;
+anotherNumber = 77;
 
 console.log(goodNumber);
 console.log(anotherNumber);
 
-let customer = {name: 'John', age: '17'};
+let customer = {
+    name: 'John',
+    age: '17'
+};
+
 let anotheCustomer = customer;
+
 anotheCustomer = {
     age: '20'
 };
