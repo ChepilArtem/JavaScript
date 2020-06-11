ed54c6a3ad684f08baeafdedd3b1cdcc68a35478
HardCode666 <hard.drive2036@gmail.com>
2020-06-11 23:38:50 +0300

    commit

---
+++ lesson2/hw1/index.js
@@ -0,0 +1,10 @@
+let a = 4;
+let b = 2;
+
+console.log("a: ", typeof a);
+console.log("b: ", typeof b);
+
+console.log('a - b', '= 2');
+console.log('a + b', '= 6');
+console.log('a / b', '= 2');
+console.log('a * b', '= 8');
