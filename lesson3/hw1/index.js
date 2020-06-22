6bc554012e891667568b6829b714ea91cb54de92
HardCode666 <hard.drive2036@gmail.com>
2020-06-22 11:05:18 +0300

    commit

--- lesson3/hw1/index.js
+++ lesson3/hw1/index.js
@@ -8,14 +8,14 @@ const anotherMessage = 'text as well';
 const isLoggedIn = Boolean(userName);
 
 /* В коде ниже нужно использовать правильные операторы после знака присваивания */
-const activeUser = anotherUser && defaultUserName;
+const activeUser = !anotherUser && defaultUserName;
 const hasAccess = isAdmin || isLoggedIn;
 const isTruthy = !message === !anotherMessage;
 const isFalsy = !isLoggedIn;
 const isNotTrue = typeof message === 'number';
 const isTrue = typeof !message === 'boolean';
 
-console.log(activeUser || defaultUserName);
+console.log(activeUser);
 console.log(hasAccess);
 console.log(isTruthy);
 console.log(isFalsy);
