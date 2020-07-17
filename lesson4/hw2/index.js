0810237955e941bde6c996a2a78edb99b7188789
HardCode666 <hard.drive2036@gmail.com>
2020-07-17 19:30:13 +0300

    commit

--- lesson4/hw2/index.js
+++ lesson4/hw2/index.js
@@ -9,9 +9,8 @@ for (let i = m; i <= n; i++) {
         result -= i;
     } else if (i % 4 == 0) {
         result *= i;
-    }
-    if (i % 5 === 0) {
-        result = i;
+    } else if (i % 5 === 0) {
+        console.log(i);
     }
 }
 
