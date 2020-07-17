c8c13993fe1296c51643bbd349aa2364f7809ba3
HardCode666 <hard.drive2036@gmail.com>
2020-07-17 13:26:49 +0300

    test

---
+++ lesson4/hw2/index.js
@@ -0,0 +1,17 @@
+let m = 2;
+let n = 10;
+let result = 0;
+
+for( m; m <= n; m++ ){
+    if( m % 2 ){
+    result += m;
+    } else 
+        if ( m % 3 ) {
+        result -= m;
+    } else 
+        if ( m % 4 ) {
+        result * m;
+    } else if ( m % 5 ) {
+        result = m;
+    }
+}
