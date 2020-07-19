c18f945515b0d4f119cb7e38d12628267425befd
HardCode666 <hard.drive2036@gmail.com>
2020-07-19 17:18:37 +0300

    commit

--- lesson5/hw2/index.js
+++ lesson5/hw2/index.js
@@ -1,24 +1,8 @@
-let getPrimes = (num) => {
-    if (num <= 0 || !Number.isInteger(num)) {
-        return null;
-    }
-    for (let i = 1; i <= num; i++) {
-        if (isPrime(i)) {
-            return i;
+function getPrimes(num){
+    for (let i = 2; i <= num; i++) {
+        for (let j = 2; j < i; j++) { 
+            if (i % j === 0) continue; 
         }
+        console.log(i);
     }
-};
-
-function isPrime(num) {
-    let count = 2;
-    while (count < num) {
-        if (num % count === 0) {
-            return false;
-        }
-        count++;
-    }
-    return true;
 }
-
-
-
