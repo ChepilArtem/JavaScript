bf5ef349820fbbcd282697bb67c4d9f5e3c2ddfb
HardCode666 <hard.drive2036@gmail.com>
2020-07-19 18:55:06 +0300

    commit

---
+++ lesson5/hw5/index.js
@@ -0,0 +1,11 @@
+function findDivCount(a, b, n){
+    let sum = 0; 
+    for(let i = a; i <= b; i++){
+        if(i % n === 0 ){
+        sum += 1; 
+    }    
+   }
+   return sum; 
+}
+
+findDivCount(3, 6, 12);
