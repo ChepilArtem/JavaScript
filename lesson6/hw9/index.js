e5c23d39b4c46afb23214d946e2676fa9111a3d2
HardCode666 <hard.drive2036@gmail.com>
2020-07-20 14:26:15 +0300

    commit

--- lesson6/hw9/index.js
+++ lesson6/hw9/index.js
@@ -7,9 +7,30 @@ let sortAsc = array => {
             if (array[j + 1] < array[j]) {
                 let min = array[j + 1];
                 array[j + 1] = array[j];
+
                 array[j] = min;
             }
         }
     }
     return array;
+}
+
+let sortDesc = array => {
+    if (!Array.isArray(array)) {
+        return null;
+    }
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = 0; j < array.length - 1 - i; j++) {
+            if (array[j + 1] < array[j]) {
+                let min = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = min;
+            }
+        }
+    }
+    let mas = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        mas.push(array[i])
+    }
+    return mas;
 }
