1f01a1b0236756391c3eae3c0d760afad9f7930a
HardCode666 <hard.drive2036@gmail.com>
2020-07-22 13:30:44 +0300

    commit

---
+++ lesson6/hw10/index.js
@@ -0,0 +1,10 @@
+function withdraw(clients, balances, client, amount) {
+    let balance = balances;
+    for (let i = 0; i < clients.length; i++) {
+        if (clients[i] == client) {
+        balance[i] = balances[i] = amount;
+        }
+        if (amount > balance[i]) return -1;
+        return balance[i];
+    }
+}
