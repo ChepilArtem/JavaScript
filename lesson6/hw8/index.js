83b611cc87dea47b1128a3dfd9972e85e866e66d
HardCode666 <hard.drive2036@gmail.com>
2020-07-22 11:15:06 +0300

    commit

---
+++ lesson6/hw8/index.js
@@ -0,0 +1,19 @@
+function checker(arr) {
+let sum = 0;
+let max = arr[0];
+let min = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) max = arr[i];
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) min = arr[i];
+    }
+
+    sum = min + max;
+    if (sum > 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
