ada280fc67fcda734cc1d9e8b21a23cb56c099ac
HardCode666 <hard.drive2036@gmail.com>
2020-07-26 22:09:57 +0300

    commit

---
+++ lesson7/hw7/index.js
@@ -0,0 +1,6 @@
+const filterNames = (arr, text) =>
+arr.filter(item => item.length > 5 && item.includes(text) ? item : false);
+
+const names = ['Jonh', 'Olivya', 'Oleksandr', 'Emanuel', 'Vanya', 'Nastya'];
+
+console.log(filterNames(names, 'ya'));
