76e6dc9bbf0594d5f35da5e8831c42a92ef0e52a
HardCode666 <hard.drive2036@gmail.com>
2020-07-26 22:13:50 +0300

    commit

---
+++ lesson7/hw8/index.js
@@ -0,0 +1,5 @@
+const arrAverage = (arr) => !Array.isArray(arr) ? null : arr.reduce((accumulator, currentValue) =>
+accumulator + currentValue) / arr.length;
+
+const arr = [2, 5, 6, 3];
+console.log(arrAverage(arr));
