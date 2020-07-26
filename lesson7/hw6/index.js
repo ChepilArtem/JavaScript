b77185d266c4a814bbc11adecc116afd15042589
HardCode666 <hard.drive2036@gmail.com>
2020-07-26 22:01:27 +0300

    commit

--- lesson7/hw6/index.js
+++ lesson7/hw6/index.js
@@ -1,6 +1,4 @@
-const sum = arr =>
-    !Array.isArray(arr) ? null :
-    arr.reduce((acc, elem) => acc + elem);
+const sum = arr => !Array.isArray(arr) ? null : arr.reduce((acc, elem) => acc + elem);
 
 const num = [8, 2, 3, 3, 5, -1]
 
