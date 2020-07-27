52c8a5c1de39bbc837019cc38329c50fdaac7767
HardCode666 <hard.drive2036@gmail.com>
2020-07-27 09:05:24 +0300

    commit

---
+++ lesson8/hw4/index.js
@@ -0,0 +1,19 @@
+const addPropertyV1 = (userData, userId) => {
+    userData.id = userId;
+    return userData;
+};
+
+const addPropertyV2 = (userData, userId) => {
+    const result = Object.assign(userData, {id: userId});
+    return result;
+};
+
+const addPropertyV3 = (userData, userId) => {
+    const result = Object.assign({}, userData,  {id: userId});
+    return result;
+};
+
+const addPropertyV4 = (userData, userId) => {
+    const result = Object.assign({...userData}, {id: userId});
+    return result;
+};
