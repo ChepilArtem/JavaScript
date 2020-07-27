2096b028807ec0baa592adbf0d147c3dc166dd80
HardCode666 <hard.drive2036@gmail.com>
2020-07-27 09:26:49 +0300

    commit

---
+++ lesson8/hw5/index.js
@@ -0,0 +1,19 @@
+const addPropertyV1 = (obj, key, value) => {
+    obj[key] = value;
+    return obj;
+};
+
+const addPropertyV2 = (obj, key, value) => {
+    const result = Object.assign(obj, {[key]: value});
+    return result;
+};
+
+const addPropertyV3 = (obj, key, value) => {
+    const result = Object.assign({}, obj,  {[key]: value});
+    return result;
+};
+
+const addPropertyV4 = (obj, key, value) => {
+    const result = Object.assign({...obj}, {[key]: value});
+    return result;
+};
