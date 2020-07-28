4216a413f38fcb2c8abe231a3166f929956969bb
HardCode666 <hard.drive2036@gmail.com>
2020-07-28 12:10:04 +0300

    commit

---
+++ lesson11/hw1/index.js
@@ -0,0 +1,20 @@
+const splitString = (text, len = 10) => {
+    if (typeof text != "string") {
+        return null;
+    }
+    let strArr = [];
+    let startPosition = 0;
+    while (true) {
+        let chunk = text.substr(startPosition, len);
+        if (chunk.length === 0) break;
+        if (chunk.length < len) {
+            while (chunk.length < len) {
+                chunk += ".";
+            }
+        }
+        strArr.push(chunk);
+        startPosition += len;
+    }
+    return strArr;
+}
+console.log(splitString('qwertyui', 9));
