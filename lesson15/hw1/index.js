6301918c491d20232bed809f0bd3427b9bfb43d8
HardCode666 <hard.drive2036@gmail.com>
2020-07-31 19:53:48 +0300

    commit

--- lesson15/hw1/index.js
+++ lesson15/hw1/index.js
@@ -26,13 +26,14 @@ export function createLogger() {
     };
 
     function getRecords(type) {
-        console.log(type)
         if (type !== undefined) {
-            let result = memory.filter(elem => elem.type === type);
+            let result = memory.filter(el => el.type === type)
+            .sort((a, b) => b.dateTime - a.dateTime);
+            console.log(result)
             return result;
         } else {
             return memory.sort((a, b) => b.dateTime - a.dateTime);
-        }; 
+        };
     };
 
     return {
