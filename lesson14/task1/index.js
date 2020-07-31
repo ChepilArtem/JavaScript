ce48d19bc6abc916070acc3257261ebeec0760f0
HardCode666 <hard.drive2036@gmail.com>
2020-07-31 13:27:39 +0300

    commit

---
+++ lesson14/task1/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let message = "Just learn it!";
+
+const sendMessage = name => {
+    const sender = 'Gromcode';
+    console.log(`${name}, ${message}, Your ${sender}`)
+};
+
+function setMessage(text) {
+    message = text;
+}
