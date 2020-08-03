d0589d0a84a2761faadcc9c8c7af65b55f754dc2
HardCode666 <hard.drive2036@gmail.com>
2020-08-03 09:28:42 +0300

    commit

---
+++ lesson17/hw4/index.js
@@ -0,0 +1,13 @@
+export const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+
+    setFullName(fullName) {
+        this.firstName = fullName.split(' ')[0];
+        this.lastName = fullName.split(' ')[1];
+    }
+}
+const func = user.getFullName.bind(user);
