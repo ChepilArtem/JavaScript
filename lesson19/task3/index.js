54566ea10e99419ca2fe1514623bd553102a4b22
HardCode666 <hard.drive2036@gmail.com>
2020-08-05 00:28:48 +0300

    commit

---
+++ lesson19/task3/index.js
@@ -0,0 +1,23 @@
+export function User(name, age) {
+    this.name = name;
+    this.age = age;
+};
+
+User.prototype.sayHi = function() {
+    console.log(`Hi, I am ${this.name}`);
+};
+
+User.prototype.requestNewPhoto = function() {
+    console.log(`New photo request was sent for ${this.name}`);
+};
+
+User.prototype.setAge = function(a) {
+    if (a < 0) return false;
+    this.age = a;
+    if (a > 0) {
+        if (a >= 25) {
+            return this.requestNewPhoto();
+        }
+        return this.age;
+    }
+};
