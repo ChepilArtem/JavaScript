ba0cc5ba48a6adb6d166458a10a27ad61ea0af5d
HardCode666 <hard.drive2036@gmail.com>
2020-08-05 00:49:04 +0300

    commit

--- lesson19/task4/index.js
+++ lesson19/task4/index.js
@@ -1,12 +1,9 @@
 export const user = {
-
     firstName: 'John',
     lastName: 'Doe',
-
     get fullName() {
         return `${this.firstName} ${this.lastName}`;
     },
-    
     set fullName(value) {
         const [firstName, lastName] = value.split(' ');
         this.firstName = firstName;
