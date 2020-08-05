35a90336471f7fdfc1ec8787a84826a53a8d6887
HardCode666 <hard.drive2036@gmail.com>
2020-08-05 12:20:50 +0300

    commit

---
+++ lesson20/hw1/index.js
@@ -0,0 +1,40 @@
+export class Order {
+    constructor(price, city, type) {
+        this.id = `${Math.random()}`;
+        this.price = price;
+        this.dateCreated = new Date();
+        this.isConfirmed = false;
+        this.dateConfirmed = null;
+        this.city = city;
+        this.type = type;
+    }
+
+    checkPrice() {
+        if (this.price < 1000) {
+            return false;
+        }
+        return true;
+    }
+
+    confirmOrder() {
+        if (!this.isConfirmed) { 
+            this.isConfirmed = true;
+            this.dateConfirmed = new Date();
+        }
+    }
+
+    isValidType() {
+        if (this.type === 'Buy' || this.type === 'Sell') {
+            return true;
+        }
+        return false;
+    }
+};
+
+const newOrder = new Order(1001, 'Buy', 'Sell');
+
+console.log(newOrder);
+// console.log(newOrder.checkPrice());
+// console.log(newOrder.confirmOrder());
+// console.log(newOrder.isValidType());
+
