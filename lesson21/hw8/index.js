12369404812f8d19d8c6c5476005036611a6bf12
HardCode666 <hard.drive2036@gmail.com>
2020-08-06 20:22:44 +0300

    commit

--- lesson21/hw8/index.js
+++ lesson21/hw8/index.js
@@ -1,18 +1,17 @@
-export const finishList = () => {
-   
-    const item1 = document.createElement('li');
-    itemOne.textContent = '1';
-    document.querySelector('ul').prepend(item1);
+export function finishList() {
+    const elem1 = document.createElement('li');
+    elem1.textContent = "1";
+    document.querySelector('ul').prepend(elem1);
 
-    const item4 = document.createElement('li');
-    itemFour.textContent = '4';
-    document.querySelector('.special').before(item4);
+    const elem4 = document.createElement('li');
+    elem4.textContent = "4";
+    document.querySelector('.special').before(elem4);
 
-    const item6 = document.createElement('li');
-    itemSix.textContent = '6';
-    document.querySelector('.special').after(item6);
+    const elem6 = document.createElement('li');
+    elem6.textContent = "6";
+    document.querySelector('.special').after(elem6);
 
-    const item8 = document.createElement('li');
-    itemEight.textContent = '8';
-    document.querySelector('ul').append(item8);
-};
+    const elem8 = document.createElement('li');
+    elem8.textContent = "8";
+    document.querySelector('ul').append(elem8);
+}
