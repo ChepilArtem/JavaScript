2f86e13f3f5d98eb916903b9ee7937ebb6019c77
HardCode666 <hard.drive2036@gmail.com>
2020-08-06 20:14:23 +0300

    commit

---
+++ lesson21/hw8/index.js
@@ -0,0 +1,18 @@
+export function finishList() {
+   
+    const item1 = document.createElement('li');
+    itemOne.textContent = '1';
+    document.querySelector('ul').prepend(item1);
+
+    const item4 = document.createElement('li');
+    itemFour.textContent = '4';
+    document.querySelector('.special').before(item4);
+
+    const item6 = document.createElement('li');
+    itemSix.textContent = '6';
+    document.querySelector('.special').after(item6);
+
+    const item8 = document.createElement('li');
+    itemEight.textContent = '8';
+    document.querySelector('ul').append(item8);
+};
