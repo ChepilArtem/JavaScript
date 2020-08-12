ac55003d3fd652ea15eed4787df4a6aa9a6ca050
HardCode666 <hard.drive2036@gmail.com>
2020-08-12 10:13:43 +0300

    commit

---
+++ lesson21/hw9/index.js
@@ -0,0 +1,13 @@
+export const finishForm = () => {
+    const formEl = document.querySelector('.login-form');
+    
+    const newInput = document.createElement('input');
+    
+    const secondInput = document.querySelector('input');
+    secondInput.setAttribute('type', 'password');
+    
+
+    newInput.setAttribute('type', 'text');
+    newInput.setAttribute('name', 'login');
+    formEl.append(newInput);
+}
