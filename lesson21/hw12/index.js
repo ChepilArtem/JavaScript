d601226a8e3b12d9ae9a11103c314ae0b79145b2
HardCode666 <hard.drive2036@gmail.com>
2020-08-13 00:51:55 +0300

    commit

--- lesson21/hw12/index.js
+++ lesson21/hw12/index.js
@@ -1,4 +1,4 @@
 export function getSection(num) {
-    const someEl = document.querySelector(`span[data-number='${num}']`)
-    return someEl.closest('.box').dataset.section;
+    const searchEl = document.querySelector(`span[data-number='${num}']`)
+    return searchEl.closest('.box').dataset.section;
 }
