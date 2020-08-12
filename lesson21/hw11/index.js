10365351e5d1b057493c936161faf9c8dcba972c
HardCode666 <hard.drive2036@gmail.com>
2020-08-13 00:46:25 +0300

    commit

---
+++ lesson21/hw11/index.js
@@ -0,0 +1,8 @@
+export const squaredNumbers = () => {
+    Array.from(document.querySelectorAll('.number'))
+        .forEach(el => {
+            el.dataset.squaredNumber = Math.pow(el.dataset.number, 2)
+        });
+}
+
+squaredNumbers();
