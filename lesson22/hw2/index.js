b22c1861fcaa2fc6316a2bafab223e427829d963
HardCode666 <hard.drive2036@gmail.com>
2020-08-13 09:35:15 +0300

    commit

---
+++ lesson22/hw2/index.js
@@ -0,0 +1,6 @@
+const eventsListElem = document.querySelector('.text-input');
+
+const logTarget = () => {
+    console.log(eventsListElem.value);
+}
+eventsListElem.addEventListener('change', logTarget);
