9c7c965954009b5acfa80a0799d8645e698cec4a
HardCode666 <hard.drive2036@gmail.com>
2020-08-26 00:55:06 +0300

    commit

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,11 +1,11 @@
 const weekDays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
-const getDayOfWeek = (date, days) => {
+export const dayOfWeek = (date, days) => {
     const day = new Date(date).getDate();
     const dateInFuture = new Date(date).setDate(day + days);
 
     return weekDays[new Date(dateInFuture).getDay()];
 };
-const result = getDayOfWeek(new Date(1992, 9, 9), 5);
+const result = dayOfWeek(new Date(1992, 9, 9), 5);
 
 console.log(result);
