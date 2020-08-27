837dc8f5c2ca28d42a8076a73ab156a4c8b6a4e1
HardCode666 <hard.drive2036@gmail.com>
2020-08-27 18:39:49 +0300

    commit

---
+++ lesson27/task1/index.js
@@ -0,0 +1,44 @@
+// getLocalStorageData
+
+localStorage.clear();
+localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' })); // <- сохраняет и делает из обьекта строку!
+localStorage.setItem('name', 'Tom');
+localStorage.setItem('age', JSON.stringify(17));
+
+
+const getLocalStorageData = () => {
+    return Object.entries(localStorage)     //Object.keys(localStorage)
+     .reduce((acc, [key, value]) => {
+         let newValue;
+         try {
+            newValue = JSON.parse(value);
+         } catch(e) {
+            newValue = value;
+         }
+         return {
+             ...acc,
+             [key]: newValue,
+         }
+     }, {});
+    
+}
+ 
+console.log(getLocalStorageData());
+
+
+// console.log(JSON.parse(localStorage.getItem('hobbies')));
+
+//input: null
+//output: obj
+// const getLocalStorageData = () => {
+//     for(let i = 0; i < localStorage.length; i++) {
+//         localStorage.key(i)
+//     }
+// }
+
+// const getLocalStorageData = () => {
+//     for(let i of localStorage.length; i++) {
+//         localStorage.key(i)
+//     }
+// }
+
